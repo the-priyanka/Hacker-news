ca1823dc13a26a44de736cea8711cf1c6b36ff3b
Priyanka <original.manaria@gmail.com>
2022-03-08 11:41:53 +0530

    display stories

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -2,15 +2,38 @@ import React from "react";
 import { useGlobalContext } from "../context";
 
 const Stories = () => {
-  const { isLoading } = useGlobalContext();
+  const { isLoading, hits } = useGlobalContext();
 
   if (isLoading) {
     return <div className="loading"></div>;
   }
   return (
-    <div>
-      <h1>stories component</h1>
-    </div>
+    <section className="stories">
+      {hits.map((story) => {
+        const { objectID, title, num_comments, url, points, author } =
+          story;
+        return (
+          <article key={objectID} className="story">
+            <h4 className="title">{title}</h4>
+            <p className="info">
+              {points} points by <span>{author} | </span>{" "}
+              {num_comments} comments
+            </p>
+            <div>
+              <a
+                href={url}
+                target="_blank"
+                className="read-link"
+                rel="noopener noreferrer"
+              >
+                read more
+              </a>
+              <button className="remove-btn">remove</button>
+            </div>
+          </article>
+        );
+      })}
+    </section>
   );
 };
 
