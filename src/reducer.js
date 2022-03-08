ef60fa4cd299d2cf7558403cc4efa96ff00d96b5
Priyanka <original.manaria@gmail.com>
2022-03-08 11:03:53 +0530

    fetch stories

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,17 @@
-import { SET_LOADING } from "./actions";
+import { SET_LOADING, SET_STORIES } from "./actions";
 
 const reducer = (state, action) => {
   switch (action.type) {
     case SET_LOADING:
       return { ...state, isLoading: true };
+
+    case SET_STORIES:
+      return {
+        ...state,
+        isLoading: false,
+        hits: action.payload.hits,
+        nbPages: action.payload.nbPages,
+      };
 
     default:
       throw new Error(`no matching "${action.type}" action type`);
